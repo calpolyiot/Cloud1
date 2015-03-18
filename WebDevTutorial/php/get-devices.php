bdf4a252cbfdfea1f2406317ddf7467b0919375c
Corey Wilson <cwilso26@calpoly.edu>
2015-03-18 11:24:47 -0700

    resubmit CLW

--- WebDevTutorial/php/get-devices.php
+++
@@ -1,4 +0,0 @@
-<?php session_start();
-   $devices = $_SESSION['userDevices'];
-   echo json_encode($devices);
-?>
--- WebDevTutorial/php/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-   session_start();
-   if(session_destroy()) // Destroying All Sessions
-   {
-      $_SESSION['loggedIn'] = false;
-      header("Location: /../index.php"); // Redirecting To Home Page
-   }
-?>
